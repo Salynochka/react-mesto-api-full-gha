1502c98a794f01f38447b9af70f2a61600b53b03
Alena Salynskaia <al_20.07@mail.ru>
2023-09-09 12:24:18 +0500

    add cors

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const { PORT = 4000 } = process.env;
 const app = express();
 
 app.use(cors({
-  origin: 'https://localhost:3000', // https://travel.network.nomoredomainsicu.ru',
+  origin: 'https://travel.network.nomoredomainsicu.ru',
   credentials: true,
 }));
 
